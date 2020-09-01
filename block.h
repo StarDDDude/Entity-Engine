71e4bbd3e0f28c674efd934c597f556b808ee5dc
stardddude <mauriceriemenschneider@gmail.com>
2020-09-01 18:39:28 +0200

    Added graphics-verticies and -indicies to block Part of: Implemenation of draw function

--- block.h
+++ block.h
@@ -8,7 +8,11 @@ public:
     int direction;
     int speed;
 
+    unsigned int VertexB_ID;
+    unsigned int Index_ID;
+
     block(int i_Xpos, int i_Ypos, int i_direction, int i_speed);        //i_ for inititializer variables
     void update();
     void back();
+    void draw();
 };
