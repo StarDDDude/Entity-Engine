a61978aa91fd13bed7ad9a5a937f3e4628dfd08a
StarDDDude <stardddude@pop-os.localdomain>
2020-09-07 12:19:53 +0200

    Fixed Graphics to draw Objects as they move Must bring draw call to an outside function

--- block.cpp
+++ block.cpp
@@ -6,12 +6,13 @@
 
 #include "GLAssert.h"
 
-block::block(int i_Xpos, int i_Ypos, int i_direction, int i_speed){
+block::block(int i_Xpos, int i_Ypos, int i_direction, int i_speed, unsigned int i_program){
     direction = i_direction;
     speed = i_speed;
     Xpos = i_Xpos;
     Ypos = i_Ypos;
     size = 5;
+    program = i_program;
 
     float g_verticies[8]{                        //g_ for graphics related variables and functions
         float(Xpos-size)/100.0f, float(Ypos-size)/100.0f,
@@ -39,80 +40,6 @@ block::block(int i_Xpos, int i_Ypos, int i_direction, int i_speed){
     GLCall(glGenBuffers(1, &Index_ID));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Index_ID));
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), g_indicies, GL_STATIC_DRAW));
-
-    //
-    //
-    //
-
-    const char* VS_src = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main(){\n"
-        "\n"
-        "gl_Position = position;\n"
-        "\n"
-        "}";
-    
-    const char* FS_src = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n"
-        "\n"
-        "void main(){\n"
-        "\n"
-        "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
-        "\n"
-        "}";
-
-
-    GLuint VS_ID, FS_ID, program;
-    program = glCreateProgram();
-
-    VS_ID = glCreateShader(GL_VERTEX_SHADER);
-    FS_ID = glCreateShader(GL_FRAGMENT_SHADER);
-
-    GLCall(glShaderSource(VS_ID, 1, &VS_src, nullptr));
-    GLCall(glCompileShader(VS_ID));
-    int result;
-    GLCall(glGetShaderiv(VS_ID, GL_COMPILE_STATUS, &result));
-    if (result == GL_FALSE){
-        int lenght;
-        GLCall(glGetShaderiv(VS_ID, GL_INFO_LOG_LENGTH, &lenght));
-        char* message = (char*)alloca(lenght * sizeof(char));
-        GLCall(glGetShaderInfoLog(VS_ID, lenght, &lenght, message));
-
-        std::cout << "shader failed to compile" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(VS_ID);
-    }
-
-
-    GLCall(glShaderSource(FS_ID, 1, &FS_src, nullptr));
-    GLCall(glCompileShader(FS_ID));
-    GLCall(glGetShaderiv(FS_ID, GL_COMPILE_STATUS, &result));
-    if (result == GL_FALSE){
-        int lenght;
-        glGetShaderiv(FS_ID, GL_INFO_LOG_LENGTH, &lenght);
-        char* message = (char*)alloca(lenght * sizeof(char));
-        glGetShaderInfoLog(FS_ID, lenght, &lenght, message);
-
-        std::cout << "shader failed to compile" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(VS_ID);
-    }
-
-    GLCall(glAttachShader(program, VS_ID));
-    GLCall(glAttachShader(program, FS_ID));
-    GLCall(glLinkProgram(program));
-    GLCall(glValidateProgram(program));
-
-    GLCall(glDeleteShader(VS_ID));
-    GLCall(glDeleteShader(FS_ID));
-
-    GLCall(glUseProgram(program));
-
 }
 
 void block::update(){
@@ -125,10 +52,14 @@ void block::back(){
     Ypos = Ypos - speed * sin(direction*M_PI/180);
 }
 
-void block::draw(){
+void block::draw(block Uniform){
     GLCall(glBindVertexArray(VertexA_ID));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Index_ID));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, VertexB_ID));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Index_ID));
+
+    GLCall(glUseProgram(program));
+    GLCall(glUniform2f(glGetUniformLocation(program, "Pos"), float(Uniform.Xpos)/100.0f, float(Uniform.Ypos)/100.0f));
 
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
 }
--- main.cpp
+++ main.cpp
@@ -32,12 +32,92 @@ int main(void){
         std::cout << "glewInit error" << std::endl;
     }
 
-    block* boi1 = new block(-30, 0, 0, 10);
-    block* boi2 = new block(80, 0, 180, 5);
+
+
+    //
+    //
+    //
+
+    const char* VS_src = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "uniform vec2 Pos;"
+        "\n"
+        "void main(){\n"
+        "\n"
+        "gl_Position = position + vec4(Pos, 0.0f,0.0f);\n"
+        "\n"
+        "}";
+    
+    const char* FS_src = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main(){\n"
+        "\n"
+        "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+        "\n"
+        "}";
+
+
+    GLuint VS_ID, FS_ID, program;
+
+
+    program = glCreateProgram();
+
+    VS_ID = glCreateShader(GL_VERTEX_SHADER);
+    FS_ID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    GLCall(glShaderSource(VS_ID, 1, &VS_src, nullptr));
+    GLCall(glCompileShader(VS_ID));
+    int result;
+    GLCall(glGetShaderiv(VS_ID, GL_COMPILE_STATUS, &result));
+    if (result == GL_FALSE){
+        int lenght;
+        GLCall(glGetShaderiv(VS_ID, GL_INFO_LOG_LENGTH, &lenght));
+        char* message = (char*)alloca(lenght * sizeof(char));
+        GLCall(glGetShaderInfoLog(VS_ID, lenght, &lenght, message));
+
+        std::cout << "shader failed to compile" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(VS_ID);
+    }
+
+
+    GLCall(glShaderSource(FS_ID, 1, &FS_src, nullptr));
+    GLCall(glCompileShader(FS_ID));
+    GLCall(glGetShaderiv(FS_ID, GL_COMPILE_STATUS, &result));
+    if (result == GL_FALSE){
+        int lenght;
+        glGetShaderiv(FS_ID, GL_INFO_LOG_LENGTH, &lenght);
+        char* message = (char*)alloca(lenght * sizeof(char));
+        glGetShaderInfoLog(FS_ID, lenght, &lenght, message);
+
+        std::cout << "shader failed to compile" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(VS_ID);
+    }
+
+    GLCall(glAttachShader(program, VS_ID));
+    GLCall(glAttachShader(program, FS_ID));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
+
+    GLCall(glDeleteShader(VS_ID));
+    GLCall(glDeleteShader(FS_ID));
+
+    GLCall(glUseProgram(program));
+
+
+
+    block* boi1 = new block(-30, 0, 0, 8, program);
+    block* boi2 = new block(80, 0, 180, 10, program);
 
     while(!glfwWindowShouldClose(window))
-    {   
-        glClear(GL_COLOR_BUFFER_BIT);
+    {
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
         boi1->update();
         boi2->update();
 
@@ -48,8 +128,18 @@ int main(void){
             boi2->direction = 0;
         }
 
-        boi1->draw();
-        boi2->draw();
+        if ((boi1->Xpos - boi1->size) < -70){
+            boi1->back();
+            boi1->direction = 0;
+        }
+        if ((boi2->Xpos + boi2->size) > 130){
+            boi2->back();
+            boi2->direction = 180;
+        }
+
+
+        boi1->draw(*boi1);                  //Create Universal Draw Function
+        boi1->draw(*boi2);                  //instead of Draw Function for every object
 
         glfwSwapBuffers(window);
         glfwPollEvents();
